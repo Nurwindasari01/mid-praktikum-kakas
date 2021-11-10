443b2a6eebd695ef2de677f3551c1a9ea0fed28e
DESKTOP-0BL7E8B\HP <nurwindasari0101@gmail.com>
2021-11-10 11:17:08 +0700

    pengertian vcs

---
+++ resources/views/pengertian vcs.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+version control system (VCS) adalah sebuah kumpulan perangkat lunak yang sudah terintegrasi dan digunakan untuk membantu software engineer mengelola perubahan dalam source code dari waktu ke waktu. 
+
+GIT
+CVS
+SVN
+Mercurial
+Monotone
+</body>
+</html>
